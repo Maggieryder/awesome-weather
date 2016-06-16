2513d51e3fb39938e6ffefeb0a0df125a3a51633
Maggieryder <maggieryder@mac.com>
2016-06-16 18:59:05 -0400

    Update dsit folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 const PORT = process.env.PORT || 3000;
-/*
+
 app.use(function(req, res, next){
   if (req.headers['x-forwarded-proto']==='https'){
     res.redirect('http://'+ req.hostname + req.url);
@@ -10,7 +10,7 @@ app.use(function(req, res, next){
     next();
   }
 });
-*/
+
 app.use(express.static('dist'));
 
 app.listen(PORT, function(){
